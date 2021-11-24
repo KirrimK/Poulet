3ea7ac10a1005757c7e2d97c7880f830b9174971
Hugo Courtadon <hugo.courtadon06@gmail.com>
2021-11-24 15:12:41 +0100

    création des fonctions show_hypo et show_prop pas encore fonctionnelles

--- cli.ml
+++ cli.ml
@@ -1,3 +1,15 @@
 (* cli.ml interface*)
 
 open Hypothese;;
+
+
+let show_hypo h = Printf.printf "%s :  %d" h.name h.id
+
+let rec show_prop = fun p ->
+match p with
+  |Name n -> Printf.printf "%s" n (* to string *) 
+  |True t-> Printf.printf "True" (* symbole mathématique*)
+  |False f->Printf.printf "False"
+  |Implies (p1,p2) -> Printf.printf " %s => %s " p1 p2
+
+
