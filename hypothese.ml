0108652e800cb0bed9c7865addd2a0bfea473448
KirrimK <KirrimK@users.noreply.github.com>
2021-11-24 13:40:53 +0100

    added gitignore and basic types

--- hypothese.ml
+++ hypothese.ml
@@ -1,1 +1,11 @@
 (* Hypothese.ml *)
+
+type proposition = Name of string
+  | Implies of proposition * proposition
+  | True
+  | False;;
+
+type hypothesis = {
+    name: string;
+    prop: proposition;
+  };;
