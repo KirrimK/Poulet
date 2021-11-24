a1401b7f15ab31ac4e5990a7d32450cddbe10fe7
KirrimK <KirrimK@users.noreply.github.com>
2021-11-24 14:31:07 +0100

    first (nontested) implementation of intro

--- strategies.ml
+++ strategies.ml
@@ -3,9 +3,13 @@
 open Hypothese;;
 
 let intro = fun proo ->
-  let nexthypname = (hd proo.hypos).id + 1 in
+  let nexthypid = (hd proo.hypos).id + 1 in
   match hd proo.remainder with
     Implies(True, _) -> (false, proo)
   | Implies(False, _) -> (false, proo)
-  | Implies(a, b) -> (true, {hypos={name=; prop=a}::proo.hypos; 
+  | Implies(a, b) -> 
+      let nexthyp = {id=nexthypid; prop=a} in
+      let nextremainder = b::(tl proo.remainder) in
+      let newproo = {hypos=(nexthyp::proo.hypos); remainder=nextremainder} in
+      (true, newproo)
   | _ -> (false, proo);;
