4219ea8b1774f222d3527a94741e13e5fe822df4
Hugo Courtadon <hugo.courtadon06@gmail.com>
2021-11-25 18:26:52 +0100

    Affichage des propositions avec une fonction rec prop_to_string et show_prop

--- cli.ml
+++ cli.ml
@@ -5,11 +5,17 @@ open Hypothese;;
 
 let show_hypo h = Printf.printf "%s :  %d" h.name h.id
 
-let rec show_prop = fun p ->
+let rec prop_to_string = fun p ->
 match p with
-  |Name n -> Printf.printf "%s" n (* to string *) 
-  |True t-> Printf.printf "True" (* symbole mathématique*)
-  |False f->Printf.printf "False"
-  |Implies (p1,p2) -> Printf.printf " %s => %s " p1 p2
+  |Name n -> n   
+  |True t-> "%s True" t
+  |False f->"%s False" f
+  |Implies (p1,p2) -> prop_to_string p1 + "=>" + prop_to_string p2 ;;
+
+ (* On peut utiliser le module Uchar pour avoir les caractères unicode mathématiques*)
+
+
+let show_prop p =  Printf.printf "%s" prop_to_string p
+
 
 
