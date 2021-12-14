e8c762937ed590690f58c69122ef06751ed72be8
Hugo Courtadon <hugo.courtadon06@gmail.com>
2021-12-14 10:02:37 +0100

    accent circonflexe pour le symbole "et"

--- cli.ml
+++ cli.ml
@@ -3,7 +3,6 @@
 open Strategies;;
 open String;;
 
-
 let show_id_hypo h = Printf.printf "%d"  h.id;;
 
 
@@ -20,18 +19,20 @@ let prop_iter = fun c_n c_t c_f f_neg f_imply f_and  prop ->
   prop_to_string  prop
 
 
- (* On peut utiliser le module Uchar pour avoir les caract�res unicode math�matiques*)
+ (* On peut utiliser le module Uchar pour avoir les caractï¿½res unicode mathï¿½matiques*)
 
 
 let c_name = fun n -> n ;;
 let c_true = "True";;
 let c_false = "False" ;;
 
+(*let and_symbol = Uchar.type  0x02C4;;*)
+
 let f_implies=fun s1 s2 -> String.concat "" ["(";s1;"=>";s2;")"];; 
 
 let f_negation=fun s -> String.concat "" ["Not";"(";s;")"];;
 
-let f_and = fun sProp1 sProp2 -> String.concat "" ["(";sProp1;"/\\";sProp2;")"];;
+let f_and = fun sProp1 sProp2 -> String.concat "" ["(";sProp1;"^";sProp2;")"];;
 
 let show_prop = fun propo -> prop_iter c_name c_true c_false f_negation f_implies f_and  propo;;
 
