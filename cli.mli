147eb516e7e48a4c13c06aff67dcb9c56ab65012
Charles Dutau <84006698+charlesdutau@users.noreply.github.com>
2021-12-17 10:04:57 +0000

    lol

--- cli.mli
+++ cli.mli
@@ -1,3 +1,23 @@
 (* cli.mli *)
 
-(*TBD*)
+val show_id_hypo : Strategies.hypothesis -> unit * unit;;
+
+val c_name : string -> string;;
+
+val c_true: string;;
+
+val c_false: string;;
+
+val f_implies: string -> string -> string;;
+
+val f_negation: string -> string;;
+
+val f_and: string -> string -> string;;
+
+val f_or: string -> string -> string;;
+
+val prop_to_string: Strategies.proposition -> string;;
+
+val print_prop: Strategies.proposition -> unit;;
+
+val string_to_list: string -> string list;;
