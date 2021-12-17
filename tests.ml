6fe68e8c3f6d4dcd4ce80e553e95f643cad151a0
KirrimK <KirrimK@users.noreply.github.com>
2021-12-17 09:00:43 +0100

    Update tests.ml

--- tests.ml
+++ tests.ml
@@ -2,9 +2,17 @@
 
 open Strategies;;
 
-let h1 = {hypos=[];remainder=[Implies(Name("A"),Name("B"))]} ;;
+let test1 = add_remainder empty_proof (make_prop ["A"; "B"; "=>"]);;
 
-if (intro h1) = (true,{hypos=[{id=0;prop=Name("A")}];remainder=[Name("B")]}) then Printf.printf "intro test 1 success\n" else Printf.printf "intro test 1 fail\n";;
+let test1_res = add_hyp (add_remainder empty_proof (make_prop ["B"])) (make_prop ["A"]);;
+
+let test = fun name funct dep arr ->
+  Printf.printf "%s test %s\n" name (if funct dep = (true, arr) then "success" else "fail");;
+
+test "intro 1" intro test1 test1_res;;
+
+(*
+if (intro test1) = (true,{hypos=[{id=0;prop=Name("A")}];remainder=[Name("B")]}) then Printf.printf "intro test 1 success\n" else Printf.printf "intro test 1 fail\n";;
 
 if (assumption {hypos=[{id=0;prop=Name("A")}];remainder=[Name("B")]}) = (false,{hypos=[{id=0;prop=Name("A")}];remainder=[Name("B")]}) then Printf.printf "assumption test 1 success\n" else Printf.printf "assumption test 1 fail\n";;
 
@@ -23,3 +31,4 @@ if (assumption h3) = (false,h3) then Printf.printf "assumption test 3 success\n"
 let h4 = {hypos=[{id=0;prop=Implies(Name("A"),Name("B"))}];remainder=[Name("B")]};;
 
 if (apply 0 h4) = (true,{hypos=[{id=0;prop=Implies(Name("A"),Name("B"))}];remainder=[Name("A")]}) then Printf.printf "apply test success\n" else Printf.printf "apply test fail\n";;
+*)
