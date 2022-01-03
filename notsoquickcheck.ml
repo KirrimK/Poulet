1d666925e2c34b6e6e76114c554a4a1c5fe3ec59
KirrimK <KirrimK@users.noreply.github.com>
2022-01-03 11:21:40 +0100

    Update notsoquickcheck.ml

--- notsoquickcheck.ml
+++ notsoquickcheck.ml
@@ -7,7 +7,17 @@
 
 
 (* Générer un contexte aléatoire *)
-let gen_rand_cont = fun 
+let add_rand_cont = fun max_prop_depth hyp_quantity proof ->
+  let rec it = fun cont acc->
+    if cont > 0 then
+      it (cont-1) (add_hyp (propAleatoire max_prop_depth) acc)
+    else
+      acc in
+  let new_proof = it hyp_quantity proof in
+  (true, proof);;
+
+let get_rand_cont = fun max_prp_dp hp->
+  add_rand_cont max_prp_dp hp empty_proof;;;
 
 (* Générer un but garanti prouvable à partir d'un contexte *)
 
