4b0ff426982a47eff0505897b56ff8c7990ab367
Victor755431 <vdecrevoisier@gmail.com>
2022-01-03 11:17:54 +0100

    déplacement de code

--- notsoquickcheck.ml
+++ notsoquickcheck.ml
@@ -1,13 +1,46 @@
 (* Module NotSoQuickCheck *)
-
+open Strategies;;
 (* Générer une proposition aléatoire *)
-
+(* propAleatoire : int -> prop *)
+let propAleatoire = fun profondeurMax->
+  (* Fonction qui génère une proposition de profondeur maximale déterminée. *)
+  let idLibre = ref 0 in 
+  if profondeurMax < 1 then raise Invalid_Input
+  else 
+  let rec iterateurLocal = fun profondeurMax ->
+    let rFloat = Random.float 1.0 in
+    if profondeurMax = 1
+      then if rFloat < 0.4 
+        then begin incr idLibre ; p_name (Printf.sprintf ("P_%d") !idLibre) end
+        else if rFloat < 0.8
+          then p_name (Printf.sprintf ("P_%d") (Random.int (!idLibre + 1)))
+          else if rFloat < 0.9 
+            then p_true
+            else p_false
+      else 
+        if rFloat < 0.04
+          then begin incr idLibre ; p_name (Printf.sprintf "P_%d" !idLibre) end
+          else if rFloat < 0.08
+            then p_name (Printf.sprintf ("P_%d") (Random.int (!idLibre + 1)))
+            else if rFloat<0.09
+              then p_true
+              else if rFloat <0.1
+                then p_false
+                else 
+                  let p1 = iterateurLocal (profondeurMax-1) in
+                  let p2 = iterateurLocal (profondeurMax-1) in
+                  if rFloat<0.4
+                  then p1 => p2
+                  else if rFloat < 0.7 
+                      then p1 ^ p2
+                      else p1 $ p2 in
+  iterateurLocal profondeurMax;;
 
 (* Générer un but aléatoire *)
 
 
-(* Générer un contexte aléatoire *)
-let gen_rand_cont = fun 
+(* Générer un contexte aléatoire 
+let gen_rand_cont = fun *)
 
 (* Générer un but garanti prouvable à partir d'un contexte *)
 
