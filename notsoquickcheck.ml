dc969fdf9c96089d7d3f867eb89322ba2b8d8865
KirrimK <KirrimK@users.noreply.github.com>
2022-01-03 11:07:25 +0100

    Update notsoquickcheck.ml

--- notsoquickcheck.ml
+++ notsoquickcheck.ml
@@ -1,10 +1,13 @@
 (* Module NotSoQuickCheck *)
 
+(* Générer une proposition aléatoire *)
+
+
 (* Générer un but aléatoire *)
 
 
 (* Générer un contexte aléatoire *)
-
+let gen_rand_cont = fun 
 
 (* Générer un but garanti prouvable à partir d'un contexte *)
 
