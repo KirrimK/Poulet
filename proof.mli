3b2fdf0513b338bf8db1814b78befa145eb189c3
Victor755431 <vdecrevoisier@gmail.com>
2022-01-04 11:53:59 +0100

    Fin de proof.mli

--- proof.mli
+++ proof.mli
@@ -12,3 +12,14 @@ val make_proof : Proposition.t list -> Proposition.t list -> t
 
 val get_hyp : int -> t -> Proposition.t
 
+val add_goal : Proposition.t -> t
+
+val add_hyp : Proposition.t -> t
+
+val get_first_goal : t -> Proposition.t
+
+val is_proven : t -> bool
+
+val remove_hyp : int -> t -> Proposition.t list
+
+val clean t -> t
