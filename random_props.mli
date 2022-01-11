e783f9902038c5a4597e1c0567dfaa0aac01b752
Charles Dutau <84006698+charlesdutau@users.noreply.github.com>
2022-01-11 23:18:08 +0000

    added some more documentation

--- random_props.mli
+++ random_props.mli
@@ -1,7 +1,24 @@
-(* notsoquickcheck.mli *)
+(* random_props.mli *)
 
 val propAleatoire : int -> Proposition.t;;
+(* Argument:
+      profondeur maximale
+   Retourne:
+      proposition aléatoire *)
 
 val add_rand_goal : int -> Proof.t -> bool * Proof.t;;
+(* Ajoute un but aléatoire à un état de preuve
+Arguments:
+   profondeur maximale
+-> état de preuve
+Retourne:
+   (réussite, nouvel état de preuve) *)
 
 val add_rand_cont : int -> int -> Proof.t -> bool * Proof.t;;
+(* Ajoute des hypothèses aléatoires à la liste d'hypothèses du but courant
+Arguments:
+   profondeur maximale
+-> nombres d'hypothèses à ajouter
+-> état de preuve
+Retourne:
+   (réussite, nouvel état de preuve) *)
